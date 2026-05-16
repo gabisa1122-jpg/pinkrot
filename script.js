37810d724616054cedb713a073590bb8aa3231fa
gabisa1122-jpg <gabisa1122@gmail.com>
2026-05-17 00:42:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ function enterSite() {
         }, 300);
     }
 }
+let startTime = Date.now();
+
+function updateRot() {
+    let seconds = (Date.now() - startTime) / 1000;
+    let body = document.body;
+
+    body.classList.remove("rot1","rot2","rot3","rot4","rot5");
+
+    if (seconds > 10) body.classList.add("rot1");
+    if (seconds > 25) body.classList.add("rot2");
+    if (seconds > 45) body.classList.add("rot3");
+    if (seconds > 70) body.classList.add("rot4");
+    if (seconds > 100) body.classList.add("rot5");
+
+    requestAnimationFrame(updateRot);
+}
+
+updateRot();
